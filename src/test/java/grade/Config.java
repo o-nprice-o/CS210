4397ee0c2b891e2b1d0fc9e83e5f15c694a3af3a
Ron Reaser <ron@reaser.org>
2024-01-31 12:35:32 -0500

    Support debugger with unit tests, no timeouts

--- src/test/java/grade/Config.java
+++ src/test/java/grade/Config.java
@@ -9,20 +9,10 @@ public class Config {
 	 * <p>
 	 * When grading, use the original number.
 	 * <p>
-	 * When debugging, you can use
-	 * any number for repeatable test cases
-	 * or null for varying test cases.
+	 * When debugging, use any fixed number for
+	 * deterministic or null for nondeterministic.
 	 */
 	static final Integer RANDOM_SEED = 2024_01;
-
-	/**
-	 * The number of milliseconds a test case
-	 * is allowed to run before a timeout.
-	 * <p>
-	 * When debugging on a slower device,
-	 * you can use a higher number.
-	 */
-	static final int TIMEOUT_MILLIS = 100;
 
 	/**
 	 * The folder where logs are generated.
