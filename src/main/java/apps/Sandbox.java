f4f89b82211d12587b363c8fc780da1e6e09f5f6
npric <npric@10.255.104.86>
2024-02-09 12:52:22 -0500

    M1 Complete

--- src/main/java/apps/Sandbox.java
+++ src/main/java/apps/Sandbox.java
@@ -8,18 +8,10 @@ import tables.LookupTable;
 
 public class Sandbox {
 	public static void main(String[] args) {
-		/*
-		 * TODO: Modify as needed to debug
-		 * or demonstrate arbitrary code.
-		 */
-
-		Table rc_table = new LookupTable("reaser_courses", List.of("title", "subject", "number", "credits", "meetings"));
-		rc_table.put("File and Data Structures", List.of("CS", "210", 4, true));
-		rc_table.put("HNRS: CS 210 Add-On", List.of("CS", "298a", 0, false));
-		rc_table.put("Principles of Programming Languages", List.of("CS", "310", 3, true));
-		rc_table.put("Database Design and Theory", List.of("CS", "440", 3, true));
-		rc_table.put("SPTP: STEM Teaching Practices", Arrays.asList("ENGR", "493a", 3, null));
-		rc_table.put("Teaching Practicum", List.of("CSEE", "490", "1 to 3", false));
-		System.out.println(rc_table);
+		Table table = new LookupTable("test", List.of("a", "b", "c"));
+		table.put("a", Arrays.asList(1, 2));
+		table.put("b", Arrays.asList(3, 4));
+		table.put("c", Arrays.asList(5, 6));
+		System.out.println(table);
 	}
 }
--- src/main/java/model/Row.java
+++ src/main/java/model/Row.java
@@ -7,6 +7,10 @@ public record Row(String key, List<Object> fields) {
 		String result = this.key + ": " + this.fields;
 		return result;
 	}
+	public int compareTo(Row other) {
+        // Compare rows based on their keys
+        return key.compareTo(other.key());
+    }
 
 	public int hashCode() {
 		return (key.hashCode() ^ fields.hashCode());
